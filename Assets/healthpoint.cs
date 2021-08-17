a5470e0735e6af0fd7d71f4983aa31b1b8cc7177
curusigawa <43631673+curusigawa@users.noreply.github.com>
2021-08-18 00:35:10 +0900

    현서

---
+++ Assets/healthpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class healthpoint : MonoBehaviour
+{
+    public Slider hpBar;
+   // private GameManager hp; 체력 레퍼런스 가져오기
+    public Text point;
+    // Start is called before the first frame update
+    void Start()
+    {
+    //    hp = gameObject.GetComponent<GameManager>(); 체력 레퍼런스 가져오기
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hpBar.value = (float) hp.health1 / (float)100; //체력 레퍼런스 가져오기
+
+        point.text =  + " /100"; //체력 레퍼런스 가져오기
+    }
+}
