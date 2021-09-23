f1015e028db648a03b952deafe0209246065c2fd
curusigawa <43631673+curusigawa@users.noreply.github.com>
2021-09-23 16:03:07 +0900

    폰트 샘플 / 기초
    .

---
+++ Assets/Script/dungeon/dungeonmanager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class dungeonmanager : MonoBehaviour
+{
+
+    public static dungeonmanager instance;
+
+    public GameObject deckbutton;
+    public GameObject showDeckPanel;
+    public GameObject charbutton;
+
+    [Header("Instruction")] // 알림창
+    public GameObject instructionBox;
+    public Text instructionText;
+
+    [Header("Notice")] // 알림창
+    public GameObject noticebox;
+    public Text noticetext;
+
+    private WaitForSeconds _UIDelay = new WaitForSeconds(2.3f);
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        showDeckPanel.SetActive(false);
+        instructionBox.SetActive(false);
+        noticebox.SetActive(false);
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void notice(string message)
+    {
+        noticetext.text = message;
+        noticebox.SetActive(false);
+        StopAllCoroutines();
+        StartCoroutine(noticeDelay());
+    }
+
+    IEnumerator noticeDelay()
+    {
+        noticebox.SetActive(true);
+        yield return _UIDelay;
+        noticebox.SetActive(false);
+    }
+
+    public void showdeck()
+    {
+        deckbutton.SetActive(false);
+        showDeckPanel.SetActive(true);
+        //카드 목록 가져오기
+    }
+
+    public void showchar()
+    {
+        charbutton.SetActive(false);
+        showDeckPanel.SetActive(true);
+        //캐릭터 목록 가져오기
+    }
+    public void backtofield()
+    {
+        deckbutton.SetActive(true);
+        showDeckPanel.SetActive(false);
+        //카드&캐릭터 정보 끄기
+    }
+}
---
+++ Assets/Script/dungeon/nodemanager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nodemanager : MonoBehaviour
+{
+    public static nodemanager instance;
+
+    public int charlocation;
+
+    void Start()
+    {
+        // charlocation = 시작위치;
+    }
+
+    public void IsCharOn() // 현재 캐릭터가 위치한 노드 표시
+    {
+
+    }
+    public void nodestatus() // 이미 탐사한 노드인지 아닌지
+    {
+        if(node.status = true)
+        {
+            node.SetActive(true);
+        }
+        else
+        {
+            node.SetActive(false);
+        }
+    }
+    public void nodeevnet() //노드 이벤트
+    {
+        if(node.eventstatus = trap)
+        {
+
+        }
+        if(node.eventstatus = cardgain)
+        {
+
+        }
+        if (node.eventstatus = cardlost)
+        {
+
+        }
+        if (node.eventstatus = cardchange)
+        {
+
+        }
+        if (node.eventstatus = heal)
+        {
+
+        }
+        if(node.eventstatus = battle)
+        {
+
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
